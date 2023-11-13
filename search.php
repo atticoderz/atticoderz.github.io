024cddd13ffdf5bd3eff530df631b306f6aafcdd
atticoderz <150189890+atticoderz@users.noreply.github.com>
2023-11-13 11:15:33 -0500

    Add files via upload

---
+++ search.php
@@ -0,0 +1,113 @@
+<!DOCTYPE html>
+<html lang="en">
+     <head>
+     <title>Recherche - Triassi & Ippolito Paysagiste Rocaille Pavé-Uni Asphalte Laval West-Island</title>
+     <meta charset="utf-8">
+     <meta name = "format-detection" content = "telephone=no" />
+     <link rel="icon" href="images/favicon.ico">
+     <link rel="shortcut icon" href="images/favicon.ico" />
+     <link rel="stylesheet" href="css/form.css">
+     <link rel="stylesheet" href="css/style.css">
+     <script src="js/jquery.js"></script>
+     <script src="js/jquery-migrate-1.1.1.js"></script>
+     <script src="js/script.js"></script> 
+     <script src="js/superfish.js"></script>
+     <script src="js/TMForm.js"></script>
+     <script src="search/search.js"></script>
+     <script src="js/jquery.equalheights.js"></script>
+     <script src="js/jquery.mobilemenu.js"></script>
+     <script src="js/jquery.easing.1.3.js"></script>
+     <script>
+
+     </script>
+
+      <!--[if lt IE 8]>
+       <div style=' clear: both; text-align:center; position: relative;'>
+         <a href="http://windows.microsoft.com/en-US/internet-explorer/products/ie/home?ocid=ie6_countdown_bannercode">
+           <img src="http://storage.ie6countdown.com/assets/100/images/banners/warning_bar_0000_us.jpg" border="0" height="42" width="820" alt="You are using an outdated browser. For a faster, safer browsing experience, upgrade for free today." />
+         </a>
+      </div>
+    <![endif]-->
+    <!--[if lt IE 9]>
+      <script src="js/html5shiv.js"></script>
+      <link rel="stylesheet" media="screen" href="css/ie.css">
+
+
+    <![endif]-->
+     </head>
+     
+     <body class="" id="top">
+     <div class="main_bg">
+     <header>
+     
+<!--==============================header=================================-->
+      <div class="container_12">
+        <div class="grid_12">
+        <h1>
+        <a href="index.html">
+          <img src="images/logo_v2.png" alt="Logo Triassi-Ippolito Paysagiste Laval">
+        </a>
+		<div class="text3">Embellissez votre aménagement paysager</div>
+      </h1>
+      <div class="clear"></div>
+      </div>
+  </div>
+     <div class="menu_block ">
+      <div class="container_12">
+        <div class="grid_12">
+            
+            
+         
+        <nav class="horizontal-nav full-width horizontalNav-notprocessed">
+            <ul class="sf-menu">
+                   <li><a href="index.html">Accueil</a></li>
+                   <li><a href="apropos-triassi-ippolito-paysagiste-laval-west-island.html">À propos</a></li>
+                   <li><a href="projets-triassi-ippolito-paysagiste-laval-west-island.html">Nos projets</a></li>
+                   <li><a href="contactez-triassi-ippolito-paysagiste-laval-west-island.html">Contactez-nous</a></li>
+				   <li><a href="en/english.html">English</a></li>
+                 </ul>
+              </nav>
+              <div class="h_search">
+              <form id="search" action="search.php" method="GET">
+            <input type="text" name="s" value="">
+            <a onClick="document.getElementById('search').submit()" class="">
+            </a>
+             <div class="clear"></div>
+            
+         </form>
+         
+         </div>
+            <div class="clear"></div>
+            </div>
+      </div>
+    
+  </div>
+</header>
+<!--==============================Content=================================-->
+<div class="content">
+  <div class="container_12">
+    <div class="grid_12 ">
+        <h3>Résultats de recherche :</h3>
+			   <div id="search-results"></div>
+    </div>
+  </div>   
+</div>
+<!--==============================footer=================================-->
+</div>
+  <footer>   
+    <div class="container_12">
+      <div class="grid_12">
+        
+         <a href="index.html" class="f_logo">
+          <img src="images/f_logo_v2.png" alt="Logo Triassi-Ippolito Landscaping Laval">
+        </a>
+		<div class="text2">Embellissez votre aménagement paysager</div>
+        <div class="clear"></div>
+        <div class="copy">
+        Tous droits réservés &copy;<span id="copyright-year"></span> Triassi & Ippolito Paysagiste inc.
+        </div>
+      </div>
+    </div>  
+  </footer>
+</body>
+</html>
